b873ec1b2431c74e05005f92a495b88f6964b7f2
LaodeGhazy <68162980+LaodeGhazy@users.noreply.github.com>
2025-02-06 12:42:03 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,43 +1,33 @@
 pipeline {
     agent any
     environment {
-        CONTAINER_NAME = "my-web-app" 
+        CONTAINER_NAME = "my-web-app" // Ganti dengan nama container Docker Anda yang sebenarnya
     }
     stages {
         stage('Checkout Code') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'server_aplikasi_ssh', url: 'https://github.com/LaodeGhazy/project1.git']]]) 
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'server_aplikasi_ssh', url: 'https://github.com/LaodeGhazy/project1.git']]]) // Kredensial Git di sini
+            }
         }
         stage('Periksa Status Aplikasi') {
             steps {
-                sh 'check_app_status.sh' 
+                sh './check_app_status.sh' // Pastikan path dan permission execute sudah benar
             }
             post {
                 failure {
                     steps {
-                        sshagent(credentials: ['server_aplikasi_ssh']) { 
-                            sh "docker restart ${my-web-app}"
+                        sshagent(credentials: ['server_aplikasi_ssh']) { // Kredensial SSH di sini
+                            sh "docker restart ${my-web-app}" // Gunakan variabel dari environment
                         }
                         // Email notifikasi (opsional)
-                        // email {
-                        //     to: 'your_email@example.com'
-                        //     subject: "Restart Service Gagal"
-                        //     body: "Restart service untuk ${CONTAINER_NAME} gagal."
-                        // }
                     }
                 }
                 success {
                     steps {
                         // Email notifikasi (opsional)
-                        // email {
-                        //     to: 'your_email@example.com'
-                        //     subject: "Restart Service Sukses"
-                        //     body: "Restart service untuk ${CONTAINER_NAME} sukses."
-                        // }
                     }
                 }
             }
         }
     }
-}
 }
