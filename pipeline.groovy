6ec2d4e9c300627aa03ded3e221d8eb41ed43ca2
LaodeGhazy <68162980+LaodeGhazy@users.noreply.github.com>
2025-02-06 12:37:39 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,23 +1,22 @@
 pipeline {
     agent any
     environment {
-        CONTAINER_NAME = "my-web-app" // Ganti dengan nama container Anda
+        CONTAINER_NAME = "my-web-app" 
     }
     stages {
         stage('Checkout Code') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'laode_ghazy', url: 'https://github.com/LaodeGhazy/project1.git']]]) // Periksa ID kredensial dan URL repository
-            }
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'server_aplikasi_ssh', url: 'https://github.com/LaodeGhazy/project1.git']]]) 
         }
         stage('Periksa Status Aplikasi') {
             steps {
-                sh 'check_app_status.sh'  // Pastikan path ini benar!
+                sh 'check_app_status.sh' 
             }
             post {
                 failure {
                     steps {
-                        sshagent(credentials: ['server_aplikasi_ssh']) { // Periksa ID kredensial SSH
-                            sh "docker restart ${CONTAINER_NAME}"
+                        sshagent(credentials: ['server_aplikasi_ssh']) { 
+                            sh "docker restart ${my-web-app}"
                         }
                         // Email notifikasi (opsional)
                         // email {
